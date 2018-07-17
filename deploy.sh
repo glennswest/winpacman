f5701542a29dffd6acec7ee07aa88699457a2859
“Glenn <glennswest@neuralcloudcomputing.com>
2018-07-17 18:18:04 -0500

    Add expose for po and svc

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ oc delete po/winpacman
 sleep 30
 oc adm new-project winpacman --node-selector=''
 oc create -f winpacman.yaml
-
-
+oc expose po/winpacman
+oc expose svc/winpacman --hostname=winpacman.app.openshift.ncc9.com
