2e8b13784dce56304bc80c60192b45d562b0086a
gwest <gwest@redhat.com>
2018-09-05 13:27:26 -0500

    Fix port

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ oc delete po/winpacman
 sleep 30
 oc adm new-project winpacman --node-selector=''
 oc create -f winpacman.yaml
-oc expose po/winpacman
+oc expose po/winpacman --port=8080
 oc expose svc/winpacman 
