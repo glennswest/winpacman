0b6e0c37379ecb6a9b19765e96f697ba2acfcb35
gwest <gwest@redhat.com>
2018-09-05 13:12:07 -0500

    Add 1803 support
    	new file:   Dockerfile.1803
    	modified:   build.bat
    	modified:   deploy.sh
    	modified:   winpacman.yaml

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ sleep 30
 oc adm new-project winpacman --node-selector=''
 oc create -f winpacman.yaml
 oc expose po/winpacman
-oc expose svc/winpacman --hostname=winpacman.app.openshift.ncc9.com
+oc expose svc/winpacman 
