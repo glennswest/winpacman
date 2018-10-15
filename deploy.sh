522c209a0aeed327f7ced02e3fd5727a0c14f485
“gwest“ <gwest@redhat.com>
2018-10-15 06:10:04 -0500

    Make sure on project

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 oc delete po/winpacman
 sleep 30
 oc adm new-project winpacman --node-selector=''
+oc project winpacman
 oc create -f winpacman.yaml
 oc expose po/winpacman --port=8080
 oc expose svc/winpacman 
